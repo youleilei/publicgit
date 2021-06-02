ba2dc8468b991cf68d1b0c20cf379804a939412b
yll <510283832@qq.com>
2021-06-02 16:57:34 +0800

    22

--- springboot-jpa/src/test/java/com/forezp/SpringbootJpaApplicationTests.java
+++ springboot-jpa/src/test/java/com/forezp/SpringbootJpaApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringbootJpaApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void contextLoadssss() {
 	}
 
 }
